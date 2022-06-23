56f16d1f25421930a3a1016e518866ac23619469
hadifsan <99936136+hadifsan@users.noreply.github.com>
2022-06-23 17:36:22 +0800

    Create For loop.py

---
+++ For loop.py
@@ -0,0 +1,12 @@
+
+friends = ["Jin", "Sued", "Fiq"]
+
+for index in range(10):
+    if index == 0:
+        print("1st iteration")
+    elif index == 1:
+        print("2nd iteration")
+    elif index == 2:
+        print("3rd iteration")
+    else:
+        print("not first")
